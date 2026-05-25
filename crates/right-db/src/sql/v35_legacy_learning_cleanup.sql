341cce3bc01cbcf05c7a6ba6dd8c0db3ca72bec4
Andrey Kuznetsov <fear@loathing.in>
2026-05-26 02:05:26 +0400

    fix(db): drop legacy learning tables

---
+++ crates/right-db/src/sql/v35_legacy_learning_cleanup.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS learning_episodes;
+DROP TABLE IF EXISTS skill_nudge_signals;
+DROP TABLE IF EXISTS skill_nudge_state;
+DROP TABLE IF EXISTS skill_review_reports;
+DROP TABLE IF EXISTS execution_events;
