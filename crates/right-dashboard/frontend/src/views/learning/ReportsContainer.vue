b0638db73eba0e717da768b4c27bc0fcb3855572
Andrey Kuznetsov <fear@loathing.in>
2026-05-31 21:16:36 +0400

    feat(dashboard): ReportsContainer self-fetches learning overview

---
+++ crates/right-dashboard/frontend/src/views/learning/ReportsContainer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { learningOverview } from '../../api'
+import { useLiveResource } from '../../composables/useLiveResource'
+import ReportsView from './ReportsView.vue'
+
+const { data } = useLiveResource(learningOverview, { key: 'learning' })
+</script>
+
+<template>
+  <ReportsView :learning="data" />
+</template>
